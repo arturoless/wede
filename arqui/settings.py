9e323bfdd2ebf27cd69ef7530611fe2f200b1048
Arturo Lessieur <arturo.lessieur@gmail.com>
2019-06-06 18:53:31 -0500

    a

--- arqui/settings.py
+++ arqui/settings.py
@@ -30,7 +30,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['*','wede.herokuapp.com']
+ALLOWED_HOSTS = []
 
 
 # Application definition
