b0711502ca57f27e506d7fac3b58b70c8dd0b769
Arturo Lessieur <arturo.lessieur@gmail.com>
2019-06-06 21:10:02 -0500

    ahora si prro

--- arqui/pi_settings.py
+++ arqui/pi_settings.py
@@ -138,6 +138,8 @@ try:
     from arqui.local_settings import *
 except ImportError:
     PI=True
+
+if PI:
     SECRET_KEY = '-(!w4wx5fc34(gk703+u40_sctk-m406q^ibl)(f27t!snw45#'
     DATABASES = {
         'default': {
